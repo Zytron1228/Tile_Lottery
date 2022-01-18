ffb765718e82d2673a761ec191e59633f19dabf0
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2022-01-17 23:08:51 -0600

    need help with changing tile colors.

--- app/src/main/java/com/example/tilelottery/MainActivity.kt
+++ app/src/main/java/com/example/tilelottery/MainActivity.kt
@@ -24,7 +24,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tile13: Button
     private lateinit var tile14: Button
     private lateinit var tile15: Button
+    private lateinit var endRound: Button
     private lateinit var balance: TextView
+//    internal lateinit var color: color
+    public var currentColor ="@color/lotDefault"
 
     private var coins = 0
     private var lot1 = Random.nextBoolean()
@@ -42,6 +45,7 @@ class MainActivity : AppCompatActivity() {
     private var lot13 = Random.nextBoolean()
     private var lot14 = Random.nextBoolean()
     private var lot15 = Random.nextBoolean()
+//    private var lot1Color = @color/lotNeither
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,61 +66,186 @@ class MainActivity : AppCompatActivity() {
         tile13 = findViewById(R.id.tile13)
         tile14 = findViewById(R.id.tile14)
         tile15 = findViewById(R.id.tile15)
+        endRound = findViewById(R.id.endButton)
         balance = findViewById(R.id.balanceCounter)
 
         tile1.setOnClickListener { view ->
-            checkLot()
+            checkLot1()
         }
         tile2.setOnClickListener { view ->
-            checkLot()
+            checkLot2()
         }
         tile3.setOnClickListener { view ->
-            checkLot()
+            checkLot3()
         }
         tile4.setOnClickListener { view ->
-            checkLot()
+            checkLot4()
         }
         tile5.setOnClickListener { view ->
-            checkLot()
+            checkLot5()
         }
         tile6.setOnClickListener { view ->
-            checkLot()
+            checkLot6()
         }
         tile7.setOnClickListener { view ->
-            checkLot()
+            checkLot7()
         }
         tile8.setOnClickListener { view ->
-            checkLot()
+            checkLot8()
         }
         tile9.setOnClickListener { view ->
-            checkLot()
+            checkLot9()
         }
         tile10.setOnClickListener { view ->
-            checkLot()
+            checkLot10()
         }
         tile11.setOnClickListener { view ->
-            checkLot()
+            checkLot11()
         }
         tile12.setOnClickListener { view ->
-            checkLot()
+            checkLot12()
         }
         tile13.setOnClickListener { view ->
-            checkLot()
+            checkLot13()
         }
         tile14.setOnClickListener { view ->
-            checkLot()
+            checkLot14()
         }
         tile15.setOnClickListener { view ->
-            checkLot()
+            checkLot15()
+        }
+        endRound.setOnClickListener { view ->
+            shuffleTiles()
         }
     }
 
-    private fun checkLot() {
+    private fun checkLot1() {
+        if (lot1) win()
+        else if (!lot1) loss()
+        //also make this tile disabled until round restarts.
+        tile1.isClickable = false
+        tile1.foregr
+
+    }
+    private fun checkLot2() {
+        if (lot2) win()
+        else if (!lot2) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot3() {
         if (
-            lot1
+            lot3
         ) win()
         else if (
-            !lot1
+            !lot3
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot4() {
+        if (
+            lot4
+        ) win()
+        else if (
+            !lot4
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot5() {
+        if (
+            lot5
+        ) win()
+        else if (
+            !lot5
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot6() {
+        if (
+            lot6
+        ) win()
+        else if (
+            !lot6
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot7() {
+        if (
+            lot7
+        ) win()
+        else if (
+            !lot7
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot8() {
+        if (
+            lot8
+        ) win()
+        else if (
+            !lot8
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot9() {
+        if (
+            lot9
+        ) win()
+        else if (
+            !lot9
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot10() {
+        if (
+            lot10
+        ) win()
+        else if (
+            !lot10
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot11() {
+        if (
+            lot11
+        ) win()
+        else if (
+            !lot11
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot12() {
+        if (
+            lot12
+        ) win()
+        else if (
+            !lot12
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot13() {
+        if (
+            lot13
+        ) win()
+        else if (
+            !lot13
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot14() {
+        if (
+            lot14
+        ) win()
+        else if (
+            !lot14
+        ) loss()
+        //also make this tile disabled until round restarts.
+    }
+    private fun checkLot15() {
+        if (
+            lot15
+        ) win()
+        else if (
+            !lot15
         ) loss()
         //also make this tile disabled until round restarts.
     }
@@ -133,19 +262,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loss() {
-        coins --
-        Toast.makeText(this, "you lost 1 coin", Toast.LENGTH_SHORT).show()
+        lossMagnitude()
+        coins -= 5
+        Toast.makeText(this, "you lost 5 coins", Toast.LENGTH_SHORT).show()
         updateScore()// and make tile red
     }
 
+    private fun lossMagnitude() {
+        TODO("Not yet implemented")
+    }
+
     private fun win() {
-        coins ++
-        Toast.makeText(this, "you won 1 coin", Toast.LENGTH_SHORT).show()
+        winMagnitude()
+        coins += 5
+        Toast.makeText(this, "you won 5 coins", Toast.LENGTH_SHORT).show()
         updateScore()// and make tile green
     }
 
+    private fun winMagnitude() {
+        TODO("Not yet implemented")
+    }
+
     private fun shuffleTiles() {
-        TODO("Not yet implemented")//randomize the tiles' value as to weather ots a win, loss, or neither, and reset all tile colors back to navy blue
+        lot1 = Random.nextBoolean()
+        lot2 = Random.nextBoolean()
+        lot3 = Random.nextBoolean()
+        lot4 = Random.nextBoolean()
+        lot5 = Random.nextBoolean()
+        lot6 = Random.nextBoolean()
+        lot7 = Random.nextBoolean()
+        lot8 = Random.nextBoolean()
+        lot9 = Random.nextBoolean()
+        lot10 = Random.nextBoolean()
+        lot11 = Random.nextBoolean()
+        lot12 = Random.nextBoolean()
+        lot13 = Random.nextBoolean()
+        lot14 = Random.nextBoolean()
+        lot15 = Random.nextBoolean()
+        Toast.makeText(this, "Tiles reshuffled.", Toast.LENGTH_SHORT).show()
+//        lot1Color =
+        //randomize the tiles' value as to weather ots a win, loss, or neither, and reset all tile colors back to navy blue
     }
 
 }
